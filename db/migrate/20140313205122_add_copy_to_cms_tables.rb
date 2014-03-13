db10d62d66b8d2b0112cb29ab84a1d6523dea267
BDNelson123 <ben.david.nelson@gmail.com>
2014-03-13 20:53:14 +0000

    added copy and copy_from to components, pages, and links

---
+++ db/migrate/20140313205122_add_copy_to_cms_tables.rb
@@ -0,0 +1,17 @@
+class AddCopyToCmsTables < ActiveRecord::Migration
+  def self.up
+    add_column :cms_links, :copy, :integer, array: true, null:false, default: '{}'
+    add_column :cms_components, :copy, :integer, array: true, null:false, default: '{}'
+    add_column :cms_pages, :copy_from, :integer
+    add_column :cms_link, :copy_from, :integer
+    add_column :cms_components, :copy_from, :integer
+  end
+
+  def self.down
+    remove_column :cms_links, :copy
+    remove_column :cms_components, :copy
+    remove_column :cms_pages, :copy_from
+    remove_column :cms_links, :copy_from
+    remove_column :cms_components, :copy_from
+  end
+end
