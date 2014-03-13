6ebffc96812c5c9507e581c78e27775c3f7f079e
BDNelson123 <ben.david.nelson@gmail.com>
2014-03-13 15:54:51 +0000

    creating copy log table

---
+++ db/migrate/20140313155003_create_cms_copies.rb
@@ -0,0 +1,13 @@
+class CreateCmsCopies < ActiveRecord::Migration
+  def change
+    create_table :cms_copies do |t|
+      t.string :type
+      t.integer :locality_from
+      t.integer :locality_to
+      t.integer :id_from
+      t.integer :id_to
+
+      t.timestamps
+    end
+  end
+end
