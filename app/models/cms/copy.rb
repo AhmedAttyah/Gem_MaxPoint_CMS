f2166c898e56c771deb33ee3727f2c8573bcb218
BDNelson123 <ben.david.nelson@gmail.com>
2014-03-13 16:03:47 +0000

    forgot to create model for copy

---
+++ app/models/cms/copy.rb
@@ -0,0 +1,7 @@
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE.rdoc', which is part of this source code package.
+
+module Cms
+  class Copy < ActiveRecord::Base
+  end
+end
