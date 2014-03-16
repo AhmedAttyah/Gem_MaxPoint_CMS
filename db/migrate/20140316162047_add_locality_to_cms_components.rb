f093e602e3c5bf31d5db5c7215cf8c0cd670e2b9
BDNelson123 <ben.david.nelson@gmail.com>
2014-03-16 16:33:31 +0000

    fixed typo in cms_components migration

--- db/migrate/20140316162047_add_locality_to_cms_components.rb
+++ db/migrate/20140316162047_add_locality_to_cms_components.rb
@@ -1,9 +1,9 @@
 class AddLocalityToCmsComponents < ActiveRecord::Migration
   def self.up
-    add_column :cms_compnents, :locality_id, :integer
+    add_column :cms_components, :locality_id, :integer
   end
 
   def self.down
-    remove_column :cms_compnents, :locality_id
+    remove_column :cms_components, :locality_id
   end
 end
