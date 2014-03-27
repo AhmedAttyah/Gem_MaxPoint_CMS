0482af4cdd7761ff4085d042104e92da4ca333a7
Benjamin Nelson <ben.david.nelson@gmail.com>
2014-03-27 13:43:29 -0700

    reverted back b/c finally fixed production issue with json parse

--- app/models/cms/geolocation.rb
+++ app/models/cms/geolocation.rb
@@ -3,10 +3,10 @@ module Cms
     @@format = 'json'
 
     def self.data(ip,type)
-      # totalUrl = "http://freegeoip.net/#{@@format}/#{ip}"
-      # response = Net::HTTP.get_response(URI.parse(totalUrl)).body
-      # info = response = JSON.parse(response)
-      return 'GB'
+      totalUrl = "http://freegeoip.net/#{@@format}/#{ip}"
+      response = Net::HTTP.get_response(URI.parse(totalUrl)).body
+      info = response = JSON.parse(response)
+      return info[:country_code]
     end
 
     def self.countryWarningList(ip)
